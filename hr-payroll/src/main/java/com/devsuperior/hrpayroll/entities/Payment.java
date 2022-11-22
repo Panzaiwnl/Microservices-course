a22c442bd0b32d131041106cadbc298eb795b831
Panzaiwnl <pedropanza4@gmail.com>
2022-11-22 10:40:10 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Payment.java
@@ -0,0 +1,48 @@
+package com.devsuperior.hrpayroll.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment implements Serializable {
+
+    @Id
+    private static final long serivalVersionUID = 1L;
+    private String name;
+    private Double dailyincome;
+    private Integer days;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getDailyincome() {
+        return dailyincome;
+    }
+
+    public void setDailyincome(Double dailyincome) {
+        this.dailyincome = dailyincome;
+    }
+
+    public Integer getDays() {
+        return days;
+    }
+
+    public void setDays(Integer days) {
+        this.days = days;
+    }
+
+    public double getTotal(){
+        return days * dailyincome;
+    }
+}
